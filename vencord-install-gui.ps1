075a5ea3368670a961afac7c32ee004e839ed7de
Jack McGinnis <42243844+MakkuMikan@users.noreply.github.com>
2024-04-18 18:28:53 +0100

    ok now final

--- vencord-install-gui.ps1
+++ vencord-install-gui.ps1
@@ -1,6 +1,7 @@
-Set-Location "$env:USERPROFILE\Downloads";
+Push-Location "$env:USERPROFILE\Downloads";
 If (!(Test-Path ".\VencordInstaller.exe") -or (Test-Path ".\VencordInstaller.exe" -OlderThan (Get-Date).AddDays(-1)))
 {
     Invoke-WebRequest "https://github.com/Vencord/Installer/releases/latest/download/VencordInstaller.exe" -OutFile "VencordInstaller.exe";
 }
 & ".\VencordInstaller.exe";
+Pop-Location
--- vencord-install.ps1
+++ vencord-install.ps1
@@ -1,6 +1,7 @@
-Set-Location "$env:USERPROFILE\Downloads";
+Push-Location "$env:USERPROFILE\Downloads";
 If (!(Test-Path ".\VencordInstallerCli.exe") -or (Test-Path ".\VencordInstallerCli.exe" -OlderThan (Get-Date).AddDays(-1)))
 {
     Invoke-WebRequest "https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli.exe" -OutFile "VencordInstallerCli.exe";
 }
 & ".\VencordInstallerCli.exe";
+Pop-Location
