3a6252e796415b0cae93ec2fe4822f49f315b25a
Jack McGinnis <42243844+MakkuMikan@users.noreply.github.com>
2024-04-18 18:13:39 +0100

    fix

--- vencord-install.ps1
+++ vencord-install.ps1
@@ -1,3 +1,6 @@
 Set-Location "$env:USERPROFILE\Downloads";
-Invoke-WebRequest "https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli.exe" -OutFile "VencordInstallerCli.exe";
+If (!(Test-Path ".\VencordInstallerCli.exe") -or (Test-Path ".\VencordInstallerCli.exe" -OlderThan (Get-Date).AddDays(-1)))
+{
+    Invoke-WebRequest "https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli.exe" -OutFile "VencordInstallerCli.exe";
+}
 & ".\VencordInstallerCli.exe";
